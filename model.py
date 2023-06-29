9182a81703b2fbc65b26d132be161df2044609ce
Chris Nosowsky <ChrisNosowsky@gmail.com>
2023-06-28 23:34:37 -0400

    Update FCNN hidden layers based on suggestions

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ import xgboost as xgb
 import postprocess
 import numpy as np
 import tensorflow as tf
+from tensorflow import keras
 from preprocess import PreProcess
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
